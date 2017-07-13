e87971ef6900abf1853b85ec4c6eb0e4bcd8450f
alainpham <apham@redhat.com>
2017-07-13 10:51:55 +0200

    update doc for openshift deployment

--- models/generic-functions/src/main/java/com/redhat/empowered/generic/helpers/CacheManagerFactory.java
+++ models/generic-functions/src/main/java/com/redhat/empowered/generic/helpers/CacheManagerFactory.java
@@ -7,6 +7,9 @@ import org.infinispan.configuration.cache.Index;
 import org.infinispan.manager.CacheContainer;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.persistence.leveldb.configuration.LevelDBStoreConfigurationBuilder;
+import org.infinispan.transaction.LockingMode;
+import org.infinispan.transaction.TransactionMode;
+import org.infinispan.transaction.lookup.GenericTransactionManagerLookup;
 
 public class CacheManagerFactory {
 	
@@ -18,10 +21,18 @@ public class CacheManagerFactory {
 		Properties indexProps = new Properties();
 		indexProps.setProperty("default.directory_provider","filesystem");
 		indexProps.setProperty("default.indexBase", indexBaseFolder);
-		builder.indexing().index(Index.LOCAL).withProperties(indexProps)
+		builder
+		//transactions
+		.transaction().transactionMode(TransactionMode.TRANSACTIONAL)
+		.transactionManagerLookup(new GenericTransactionManagerLookup())
+		.lockingMode(LockingMode.PESSIMISTIC) 
+		//indexing
+		.indexing().index(Index.LOCAL).withProperties(indexProps)
+		//persistence
 		.persistence().addStore(LevelDBStoreConfigurationBuilder.class)
         .location(persistenceFolder + "/ldb")
-        .expiredLocation(persistenceFolder + "/ldb-exp").build();
+        .expiredLocation(persistenceFolder + "/ldb-exp").build()
+        ;
 		
 	}
 	
